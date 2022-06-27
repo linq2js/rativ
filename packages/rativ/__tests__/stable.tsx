f15640fc8ba21e16732bfa217c6569b12ebca3e0
hung.nguyen <linqtojs@gmail.com>
2022-06-27 11:45:52 +0700

    test: add stable unittest

--- packages/rativ/__tests__/stable.tsx
+++ packages/rativ/__tests__/stable.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import { defaultProps, stable } from "../lib/main";
+import { fireEvent, render } from "@testing-library/react";
+import { defaultProps, signal, stable } from "../lib/main";
 
 test("default props", () => {
   const Component = stable((props: { message?: string }) => {
@@ -10,4 +10,20 @@ test("default props", () => {
   });
   const { getByTestId } = render(<Component />);
   expect(getByTestId("output").textContent).toBe("Hello World");
+});
+
+test("counter", () => {
+  const Component = stable(() => {
+    const count = signal(0);
+    const increment = () => count.set((prev) => prev + 1);
+    return () => (
+      <button data-testid="output" onClick={increment}>
+        {count.get()}
+      </button>
+    );
+  });
+  const { getByTestId } = render(<Component />);
+  expect(getByTestId("output").textContent).toBe("0");
+  fireEvent.click(getByTestId("output"));
+  expect(getByTestId("output").textContent).toBe("1");
 });
